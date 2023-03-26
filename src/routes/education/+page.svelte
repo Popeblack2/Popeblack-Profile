1206973fff56aaf033ffe21dba32a9348ee5ad89
Popeblack2 <69740780+Popeblack2@users.noreply.github.com>
2023-03-26 22:45:16 +0100

    1 file modified

--- src/routes/education/+page.svelte
+++ src/routes/education/+page.svelte
@@ -1,10 +1,10 @@
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+	<title>Education</title>
+	<meta name="description" content="Educational Background" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>I'm a graduate of Bayero University Kano, Nigeria</h1>
 
 	<p>
 		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
