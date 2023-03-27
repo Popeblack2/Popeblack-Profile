33a500ec46bfdc5dfc0f3fc367ddb3eb7f5753b1
Popeblack2 <69740780+Popeblack2@users.noreply.github.com>
2023-03-27 22:30:27 +0100

    1 file modified

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,21 +1,24 @@
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+	<title>experiences</title>
+	<meta name="description" content="experience" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>My experiences with Projects</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+	<<!DOCTYPE html>
+<html>
+<head>
+	<title>Secret Network Africa Lead / Secret Agent Coordinator</title>
+</head>
+<body>
+	<h1>Secret Network Africa Lead / Secret Agent Coordinator</h1>
+	<p>Successfully planned, attended, and executed over 50 online and offline events (AMA, Webinars, Twitter Space, Conferences, and Summit)</p>
+	<p>Managing and Coordinating Discord Server</p>
+	<p>Contributing to idealizing, implementing, executing, community building strategy, Agent program improvement with the core team, and partnership with various blockchain projects.</p>
+	<p>Generating Leads</p>
+	<p>Setting up workshops</p>
+	<p>Managing the African community</p>
+</body>
+</html>
 </div>
