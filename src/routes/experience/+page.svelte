1baf36be99d27b81fe4a8f6fa32b2c09318e2d75
Popeblack2 <69740780+Popeblack2@users.noreply.github.com>
2023-03-27 22:48:25 +0100

    1 file modified

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -5,18 +5,13 @@
 
 <div class="text-column">
 	<h1>My experiences with Projects</h1>
-	
-<head>
-	<title>Secret Network Africa Lead / Secret Agent Coordinator</title>
-</head>
-<body>
-	<h1>Secret Network Africa Lead / Secret Agent Coordinator</h1>
+
+	<h2>Secret Network Africa Lead / Secret Agent Coordinator</h2>
 	<p>Successfully planned, attended, and executed over 50 online and offline events (AMA, Webinars, Twitter Space, Conferences, and Summit)</p>
 	<p>Managing and Coordinating Discord Server</p>
 	<p>Contributing to idealizing, implementing, executing, community building strategy, Agent program improvement with the core team, and partnership with various blockchain projects.</p>
 	<p>Generating Leads</p>
 	<p>Setting up workshops</p>
 	<p>Managing the African community</p>
-</body>
-</html>
+
 </div>
