ef7b5968bcc932f4dfe44b34b978ce9bf305314f
Popeblack2 <69740780+Popeblack2@users.noreply.github.com>
2023-03-27 23:20:51 +0100

    1 file modified

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -6,47 +6,48 @@
 <div class="text-column">
 	<h1>My experiences with Projects</h1>
 
-	<h2>Secret Network Africa Lead / Secret Agent Coordinator</h2>
-	<style>
-		body {
-			font-family: Arial, sans-serif;
-			font-size: 16px;
-			line-height: 1.5;
-			margin: 0;
-			padding: 0;
-		}
-		h1 {
-			text-align: center;
-			margin: 20px 0;
-		}
-		p {
-			margin: 0 0 10px 20px;
-			list-style-type: disc;
-			list-style-position: inside;
-		}
-	</style>
+	<title>Secret Network Africa Lead / Secret Agent Coordinator</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 16px;
+            line-height: 1.5;
+            margin: 0;
+            padding: 0;
+        }
+        h1 {
+            text-align: center;
+            margin: 20px 0;
+        }
+        li {
+            margin: 0 0 10px 20px;
+            list-style-type: disc;
+            list-style-position: inside;
+        }
+    </style>
 </head>
 <body>
-	<h1>Secret Network Africa Lead / Secret Agent Coordinator</h1>
-	<ul>
-		<li>
-			<p>Successfully planned, attended, and executed over 50 online and offline events (AMA, Webinars, Twitter Space, Conferences, and Summit)</p>
-		</li>
-		<li>
-			<p>Managing and Coordinating Discord Server</p>
-		</li>
-		<li>
-			<p>Contributing to idealizing, implementing, executing, community building strategy, Agent program improvement with the core team, and partnership with various blockchain projects.</p>
-		</li>
-		<li>
-			<p>Generating Leads</p>
-		</li>
-		<li>
-			<p>Setting up workshops</p>
-		</li>
-		<li>
-			<p>Managing the African community</p>
-		</li>
-	</ul>
+    <h1>Secret Network Africa Lead / Secret Agent Coordinator</h1>
+    <ul>
+        <li>
+            Successfully planned, attended, and executed over 50 online and offline events (AMA, Webinars, Twitter Space, Conferences, and Summit)
+        </li>
+        <li>
+            Managing and Coordinating Discord Server
+        </li>
+        <li>
+            Contributing to idealizing, implementing, executing, community building strategy, Agent program improvement with the core team, and partnership with various blockchain projects.
+        </li>
+        <li>
+            Generating Leads
+        </li>
+        <li>
+            Setting up workshops
+        </li>
+        <li>
+            Managing the African community
+        </li>
+    </ul>
+</body>
 
 </div>
