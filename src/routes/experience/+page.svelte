a0dd29b6a6204f402752a4a72db7b410a0bc50f1
Popeblack2 <69740780+Popeblack2@users.noreply.github.com>
2023-03-27 23:04:37 +0100

    1 file modified

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -7,11 +7,46 @@
 	<h1>My experiences with Projects</h1>
 
 	<h2>Secret Network Africa Lead / Secret Agent Coordinator</h2>
-	<p>Successfully planned, attended, and executed over 50 online and offline events (AMA, Webinars, Twitter Space, Conferences, and Summit)</p>
-	<p>Managing and Coordinating Discord Server</p>
-	<p>Contributing to idealizing, implementing, executing, community building strategy, Agent program improvement with the core team, and partnership with various blockchain projects.</p>
-	<p>Generating Leads</p>
-	<p>Setting up workshops</p>
-	<p>Managing the African community</p>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			font-size: 16px;
+			line-height: 1.5;
+			margin: 0;
+			padding: 0;
+		}
+		h1 {
+			text-align: center;
+			margin: 20px 0;
+		}
+		p {
+			margin: 0 0 10px 20px;
+			list-style-type: disc;
+			list-style-position: inside;
+		}
+	</style>
+</head>
+<body>
+	<h1>Secret Network Africa Lead / Secret Agent Coordinator</h1>
+	<ul>
+		<li>
+			<p>Successfully planned, attended, and executed over 50 online and offline events (AMA, Webinars, Twitter Space, Conferences, and Summit)</p>
+		</li>
+		<li>
+			<p>Managing and Coordinating Discord Server</p>
+		</li>
+		<li>
+			<p>Contributing to idealizing, implementing, executing, community building strategy, Agent program improvement with the core team, and partnership with various blockchain projects.</p>
+		</li>
+		<li>
+			<p>Generating Leads</p>
+		</li>
+		<li>
+			<p>Setting up workshops</p>
+		</li>
+		<li>
+			<p>Managing the African community</p>
+		</li>
+	</ul>
 
 </div>
