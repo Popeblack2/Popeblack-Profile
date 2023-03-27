c36d108d85380b96b9452e0f38fdf0edf83db37d
Popeblack2 <69740780+Popeblack2@users.noreply.github.com>
2023-03-27 22:36:30 +0100

    2 files modified

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>visit <a href="https://twitter.com/thepopeblack">Popeblack</a> to work with me</p>
 	</footer>
 </div>
 
